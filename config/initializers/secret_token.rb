3e81dbc19188c43910e4f98da9652c48ad9bc5c0
David Rowe <david@lanworx.com.au>
2012-10-20 22:26:21 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd74778a122e6bbbc898f55960b47f149edd0afea4617a27d3cee8cf4233884b5dc27c7b2a9f7f4414c90767836bbaaf2464689b3dd245bd0897c9f0658ac105a'
